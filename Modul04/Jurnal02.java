e11b93848f436b9a0c82c1260be21d4823d22be2
Alif Siregar <alifsiregar2007@gmail.com>
2025-10-09 18:14:04 +0700

    Jurnal 2-3

--- Modul04/Jurnal02.java
+++ Modul04/Jurnal02.java
@@ -1,6 +1,40 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int penghasilanJuta = input.nextInt();
+
+        double pajakPersen;
+        int Pajakbulat;
+        if (penghasilanJuta > 500) {
+            pajakPersen = 0.30;
+            Pajakbulat = 30;
+        } else if (penghasilanJuta > 250) {
+            pajakPersen = 0.25;
+            Pajakbulat = 25;
+        } else if (penghasilanJuta > 50) {
+            pajakPersen = 0.15;
+            Pajakbulat = 15;
+        } else if (penghasilanJuta > 0) {
+            pajakPersen = 0.05;
+            Pajakbulat = 5;
+        } else {
+            pajakPersen = 0;
+            Pajakbulat = 0;
+            
+            
+
+        }
+
+        long penghasilanRupiah = (long) penghasilanJuta * 1000000L;
+        long pajakBayar = (long) (penghasilanRupiah * pajakPersen);
+        long penghasilanBersih = penghasilanRupiah - pajakBayar;
+
+        System.out.println("Penghasilan Kotor = " + penghasilanRupiah);
+        System.out.println("Pajak " + Pajakbulat + "% = Rp. " + pajakBayar);
+        System.out.println("Penghasilan Bersih = Rp. " + penghasilanBersih);
     }
 }
+
--- Modul04/Jurnal03.java
+++ Modul04/Jurnal03.java
@@ -1,6 +1,34 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int d = input.nextInt();
+        int r = input.nextInt();
+        int i = input.nextInt();
+
+        if (d < r && d < i){
+            if (r < i){
+            System.out.println("Dira, Radi, Idar");
+            }else if (i < r){
+                System.out.println("Dira, Idar, Radi");
+            }
+        } else if (r < i && r < d){
+            if (i < d){
+                System.out.println("Radi, Idar, Dira");
+            }else if (d < i){
+                System.out.println("Radi, Idar, Dira");
+            }
+            
+        } else if (i < d && i < r){
+            if (d < r){
+
+                System.out.println("Idar, Dira, Radi");
+            }else if (r < d){
+                System.out.println("Idar, Radi, Dira");
+            }
+            
+        }
     }
 }

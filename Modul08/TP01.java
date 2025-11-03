0fb6b861116f7e755c101c12ba3d2098b1f50c33
Alif Siregar <alifsiregar2007@gmail.com>
2025-11-03 07:00:26 +0700

    Modul08

--- Modul08/TP01.java
+++ Modul08/TP01.java
@@ -1,6 +1,20 @@
-public class TP01 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long N = input.nextLong(); 
+        input.close();
+
+        long amoeba = 30; 
+        int waktu = 0;    
+
+        
+        while (amoeba < N) {
+            amoeba *= 2;    
+            waktu += 15;    
+        }
+
+        System.out.println(waktu);
     }
 }
--- Modul08/TP03.java
+++ Modul08/TP03.java
@@ -1,6 +1,19 @@
-public class TP03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int sum = 0;                          
+        int x = sc.nextInt();                 
+
+        
+        while (x != 0) {
+            sum += x;        
+            x = sc.nextInt(); 
+        }
+
+        System.out.println(sum); 
+        sc.close();              
     }
 }
+

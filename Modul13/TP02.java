b749e6efe53c334c882513f1f8bbb080b812f925
M. Rasyid Asyran <rasyidmuahamad@gmail.com>
2025-12-08 21:34:15 +0700

    Modul13

--- Modul13/TP02.java
+++ Modul13/TP02.java
@@ -1,6 +1,82 @@
+import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Buat Array
+        double[] gelombang1 = new double[10];
+        double[] gelombang2 = new double[10];
+
+        // Membaca input untuk gelombang1
+        for (int i = 0; i < 10; i++) {
+            gelombang1[i] = input.nextDouble();
+        }
+
+        // Membaca input untuk gelombang2
+        for (int i = 0; i < 10; i++) {
+            gelombang2[i] = input.nextDouble();
+        }
+
+        // Proses Sorting
+        bubbleSort1(gelombang1);
+        bubbleSort2(gelombang2);
+
+        // Output
+        // Cetak gelombang1
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%.2f", gelombang1[i]);
+            if (i < 9) {
+                System.out.printf(" ");
+            }
+        }
+
+        System.out.println();
+
+        // Cetak gelombang2
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%.2f", gelombang2[i]);
+            if (i < 9) {
+                System.out.printf(" ");
+            }
+        }
+
+        System.out.println();
+    }
+
+
+
+    // Method Bubble Sort untuk gelombang1
+    public static void bubbleSort1(double[] gelombang1) {
+        
+        for (int i = 1; i < 10; i++) {
+            // Membandingkan elemen berurutan
+            for (int j = 10 - 1; j >= i; j--) {
+                if (gelombang1[j] > gelombang1[j - 1]) {
+                    // Menukar elemen jika elemen saat ini lebih kecil
+                    double temp = gelombang1[j];
+                    gelombang1[j] = gelombang1[j - 1];
+                    gelombang1[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Method Bubble Sort untuk gelombang2
+    public static void bubbleSort2(double[] gelombang2) {
+        
+        for (int i = 1; i < 10; i++) {
+            // Membandingkan elemen berurutan
+            for (int j = 10 - 1; j >= i; j--) {
+                if (gelombang2[j] > gelombang2[j - 1]) {
+                    // Menukar elemen jika elemen saat ini lebih kecil
+                    double temp = gelombang2[j];
+                    gelombang2[j] = gelombang2[j - 1];
+                    gelombang2[j - 1] = temp;
+                }
+            }
+        }
     }
 }
--- Modul13/TP03.java
+++ Modul13/TP03.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input jumlah mahasiswa
+        int jumlahMahasiswa = input.nextInt();
+
+        // Buat Array
+        int[] array = new int[jumlahMahasiswa];
+
+        // Membaca input untuk array
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            array[i] = input.nextInt();
+        }
+
+        // Input key yang dicari
+        int key = input.nextInt();
+
+        // Variabel untuk menyimpan indeks yang ditemukan
+        boolean found = false;
+
+        // Linear search
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            if (array[i] == key) {
+                found = true;
+                System.out.print("Ditemukan di indeks ke-" + i); // Cetak indeks
+            }
+        }
+
+        // Jika nilai tidak ditemukan
+        if (!found) {
+            System.out.println("Tidak ditemukan");
+        }
     }
 }

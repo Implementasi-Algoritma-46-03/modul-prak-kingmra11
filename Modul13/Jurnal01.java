50ada3307919c71050b7176030f01aaef732bbae
M. Rasyid Asyran <rasyidmuahamad@gmail.com>
2025-12-23 11:44:34 +0700

    Assessment

--- Modul13/Jurnal01.java
+++ Modul13/Jurnal01.java
@@ -5,27 +5,40 @@ public class Jurnal01 {
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        String[] b = new String[a];
 
-        for (int i = 0; i < b.length; i++) { // membaca setiap kata dan memasukkannya ke dalam array
-            b[i]= scan.next().trim(); // menghapus spasi berlebihan didepan satu atau belakang
+        int a = scan.nextInt();       // jumlah kata
+        String[] b = new String[a];   // array input
+        String[] hasil = new String[a]; // array hasil decode
+
+        for (int i = 0; i < b.length; i++) {
+            b[i] = scan.next().trim(); // baca tagar
         }
 
         for (int i = 0; i < b.length; i++) {
-            String key = b[i]; // elemen yang sedang diurutkan 
-            int j = i - 1; 
+            // hilangkan tanda #
+            String kata = b[i].substring(1);
 
-            while(j >= 0 && b[j].compareToIgnoreCase(key) < 0){ // membandingkan string tanpa melihat besar kecil hurufnya
-                b[j + 1] = b[j]; // jika keil maka
-                j -= 1;
+            // reverse kata
+            String balik = "";
+            for (int j = kata.length() - 1; j >= 0; j--) {
+                balik += kata.charAt(j);
             }
 
-            b[j + 1] = key;
+            // ambil huruf kapital saja
+            String bersih = "";
+            for (int j = 0; j < balik.length(); j++) {
+                char c = balik.charAt(j);
+                if (c >= 'A' && c <= 'Z') {
+                    bersih += c;
+                }
+            }
+
+            hasil[i] = bersih;
         }
 
-        for (int i = 0; i < b.length; i++) {
-            System.out.printf("%s ", b[i]);
+        // output hasil
+        for (int i = 0; i < hasil.length; i++) {
+            System.out.printf("%s ", hasil[i]);
         }
 
         scan.close();

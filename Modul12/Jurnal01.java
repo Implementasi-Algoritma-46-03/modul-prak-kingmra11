6a89b87c0219fb124642f6e66c8320f573ad348f
M. Rasyid Asyran <rasyidmuahamad@gmail.com>
2025-12-23 11:44:48 +0700

    Assessment

--- Modul12/Jurnal01.java
+++ Modul12/Jurnal01.java
@@ -7,19 +7,19 @@ public class Jurnal01 {
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
 
-        int[][] matriks = new int[N][N];
+        int[][] matriks = new int[N][N]; // untuk baris dan kolom
 
-        for (int i=0; i<N; i++) {
+        for (int i=0; i<N; i++) { // untuk memasukan angka dimulai dari baris kiri ke kanan
             for (int j=0; j<N; j++) {
-                matriks[i][j] = s.nextInt();
+                matriks[i][j] = s.nextInt(); // semisal [0][0]= matriks 1
             }
         }
 
-        for (int i=N-1; i>=0; i--) {
+        for (int i=N-1; i>=0; i--) { // untuk membalikan angka yang awaknya berurutan jadi terbalik
             for (int j=0; j<N; j++) {
                 System.out.print(matriks[i][j]);
                 if (j < N -1)
-                    System.out.print(" ");
+                    System.out.print(" "); // untuk memberi spasi tiap angka
             }
             System.out.println();     
         }
